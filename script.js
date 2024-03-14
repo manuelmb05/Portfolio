66d93bf6e37972c38169ad870863ec132928823a
manuelmb05 <manuel.mb@outlook.com>
2024-03-14 17:37:18 -0400

    TEXT 22

---
+++ script.js
@@ -0,0 +1,6 @@
+function showAnimation() {
+    // Obtener el contenedor de imágenes adicionales
+    var additionalImages = document.querySelector('.additional-images');
+    // Mostrar el contenedor de imágenes adicionales
+    additionalImages.style.display = 'block';
+}
